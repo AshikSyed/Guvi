dc7604fba4d0f2bd5d1dd48c5e0a49c9ca2bde1b
AshikSyed <ashiksyedbe@gmail.com>
2016-09-03 10:10:34 +0530

    OdEv.java is created

---
+++ OdEv.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class OdEv
+{
+  public static void main(String[] args)
+  {
+   Scanner ss=new Scanner(System.in);
+   int n=ss.nextInt();
+   if(n%2==0)
+   {
+    System.out.println("even");
+    }
+    else
+    {
+    System.out.println("odd");
+    }
+    }
+    }
